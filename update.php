3ef6822bc775fef93b07f7cf07636a0123e3cc50
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 16:50:50 +0200

    Update update.php

--- update.php
+++ update.php
@@ -5,6 +5,13 @@
     <link href="https://fonts.googleapis.com/css?family=Fira+Code:400,600&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
+    <style>
+      .updateStatus {
+      color: #ffffff;
+      text-decoration: none;
+      font-size: 30px;
+      }
+    </style>
     <title>Updating piRa1n...</title>
   </head>
   <body>
@@ -13,7 +20,7 @@
     </p>
     <div class="output">
        <strong>⚠️ Don't shutdown the Pi until the update are installed!</strong><br>
-       You can find the status of the update <a href="update_status.php">here</a>.
+       You can find the status of the update <a href="update_status.php" class="updateStatus">here</a>.
       <?php
       if(isset($_POST['updateSubmit'])){
         shell_exec("nohup sudo /home/pi/piRa1n-web/update.sh > /dev/null 2>&1 &");
