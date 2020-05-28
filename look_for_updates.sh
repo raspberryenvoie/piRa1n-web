78ba762840f94978e2510b043bbdaecf85e19c7d
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 12:58:00 +0200

    Update look_for_updates.sh

--- look_for_updates.sh
+++ look_for_updates.sh
@@ -2,7 +2,7 @@
 wget -q --spider https://google.com
 if [ $? -eq 0 ]; then
   if [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) == "$(< /home/pi/piRa1n/version)" ]]; then
-    echo 'The system is up to date!'
+    echo 'piRa1n is up to date!'
   elif [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) != "$(< /home/pi/piRa1n/version)" ]]; then
     echo 'An update is available!'
   fi
