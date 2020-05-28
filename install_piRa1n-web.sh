80246ecce7f6dcee2bb95b85e1dbfeb919fa498b
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 13:17:22 +0200

    Update install_piRa1n-web.sh

--- install_piRa1n-web.sh
+++ install_piRa1n-web.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt update
 sudo apt upgrade -y
 sudo apt install apache2 php git -y
@@ -8,4 +8,4 @@ cd /home/pi/
 git clone https://github.com/raspberryenvoie/piRa1n-web.git
 cd piRa1n-web/
 sudo cp index.php options.php shutdown.php style.css stylesheet.css update.php update_status.php /var/www/html/
-echo -e "\n# piRa1n-web\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/config.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/shutdown.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/update.sh\n# End of piRa1n-web" | sudo EDITOR='tee -a' visudo
+echo -e "\n# piRa1n-web\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/config.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/shutdown.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n-web/update.sh\n# End of piRa1n-web" | sudo EDITOR='tee -a' visudo
