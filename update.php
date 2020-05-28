6c7e2352a6e73bf8ea22b100bd4b6b49f6c90424
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 12:46:06 +0200

    Update update.php

--- update.php
+++ update.php
@@ -13,7 +13,7 @@
     </p>
     <div class="output">
        <strong>⚠️ Don't shutdown the Pi until the update are installed!</strong><br>
-       You can find the status of the update <a href="update_status.php>here</a>.
+       You can find the status of the update <a href="update_status.php">here</a>.
       <?php
       if(isset($_POST['updateSubmit'])){
         shell_exec("nohup sudo /home/pi/piRa1n-web/update.sh > /dev/null 2>&1 &");
