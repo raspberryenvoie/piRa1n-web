42e40858090b7aebd5d22796cd8e6a69654c9385
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 16:26:28 +0200

    Update look_for_updates.sh

--- look_for_updates.sh
+++ look_for_updates.sh
@@ -2,10 +2,10 @@
 wget -q --spider https://google.com
 if [ $? -eq 0 ]; then
   if [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) == "$(< /home/pi/piRa1n/version)" ]]; then
-    echo 'piRa1n is up to date!'
+    echo '0'
   elif [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) != "$(< /home/pi/piRa1n/version)" ]]; then
-    echo 'An update is available!'
+    echo '1'
   fi
 else
-    echo "Offline"
+    echo "2"
 fi
