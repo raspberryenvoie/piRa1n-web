10697ee01b3f39122434172ddd6573e5b6c7c6c5
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 11:52:28 +0200

    Update install_piRa1n-web.sh

--- install_piRa1n-web.sh
+++ install_piRa1n-web.sh
@@ -8,4 +8,4 @@ cd /home/pi/
 git clone https://github.com/raspberryenvoie/piRa1n-web.git
 cd piRa1n-web/
 sudo cp index.php options.php shutdown.php style.css stylesheet.css update.php update_status.php /var/www/html/
-printf "www-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/config.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/shutdown.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/update.sh" | sudo EDITOR='tee -a' visudo
+echo -e "\n# piRa1n-web\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/config.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/shutdown.sh\nwww-data ALL=(ALL) NOPASSWD: /home/pi/piRa1n/update.sh\n# End of piRa1n-web" | sudo EDITOR='tee -a' visudo
