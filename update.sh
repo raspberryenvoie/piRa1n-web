81bc4091429f165e2ea35750d55fb777425dfca0
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-05-28 12:45:20 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -q --spider https://google.com
+if [ $? -eq 0 ]; then
+  if [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) == "$(< /home/pi/piRa1n/version)" ]]; then
+    echo 'The system is up to date!'
+  elif [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) != "$(< /home/pi/piRa1n/version)" ]]; then
+    echo 'An update is available!'
+  fi
+else
+    echo "Offline"
+fi
