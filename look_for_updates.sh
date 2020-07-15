7ad1ab40aaa9bd59d1cb81ac6c0d2a75b151b19f
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-07-15 20:31:46 +0200

    Update look_for_updates.sh

--- look_for_updates.sh
+++ look_for_updates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -q --spider https://google.com
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   if [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) == "$(< /home/pi/piRa1n/version)" ]]; then
     echo '0'
   elif [[ $(curl -sk https://raw.githubusercontent.com/raspberryenvoie/piRa1n/master/version) != "$(< /home/pi/piRa1n/version)" ]]; then
