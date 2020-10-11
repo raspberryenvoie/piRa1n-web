1284cce715f7fcd31d7d699b15a089e083439c48
raspberryenvoie <raspberryenvoie@protonmail.com>
2020-10-11 11:18:15 +0200

    Fix color

--- install_piRa1n-web.sh
+++ install_piRa1n-web.sh
@@ -2,12 +2,12 @@
 [[ $EUID -ne 0 ]] && { echo 'Please run as root'; exit 1; }
 [[ -d /home/pi/piRa1n/ ]] || { echo 'Install piRa1n first'; exit 1; }
 
-echo -e "\033[0;32m#########################################"
+echo -e "\033[0;32m#########################################\033[0m"
 echo -e "\033[0;32m#                                       #\033[0m"
 echo -e "\033[0;32m#  \033[0;36mWelcome to the piRa1n-web installer  \033[0;32m#\033[0m"
-echo -e "\033[0;32m#        \033[0;36mMade by raspberryenvoie        \033[0;32m#"
-echo -e "\033[0;32m#                                       #"
-echo -e "\033[0;32m#########################################"
+echo -e "\033[0;32m#        \033[0;36mMade by raspberryenvoie        \033[0;32m#\033[0m"
+echo -e "\033[0;32m#                                       #\033[0m"
+echo -e "\033[0;32m#########################################\033[0m"
 
 read -p 'Installing piRa1n-web will delete the whole contents of /var/www/html/'$'\n''Do you want to continue? [Y/n] ' continue
 case $continue in
